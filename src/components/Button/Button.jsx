103fd93a7c5df9a5b487dd47b696848ccaa98423
Aldo Halim <aldohalim28@gmail.com>
2020-10-11 15:03:32 +0800

    [feature] add button component

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import cx from "classnames";
+
+import styles from "./styles.scss";
+
+const Button = ({ className, children, size, ...rest }) => {
+  const classnames = cx(
+    {
+      [styles.button]: true,
+      [styles.small]: size === "small",
+      [styles.medium]: size === "medium",
+      [styles.large]: size === "large",
+    },
+    className
+  );
+  return (
+    <button type="button" className={classnames} {...rest}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
