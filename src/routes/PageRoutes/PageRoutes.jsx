e1289186a87c9f2f1b4add164b76834b3519c210
Aldo Halim <aldohalim28@gmail.com>
2020-10-13 14:44:45 +0800

    [feature] add course list page, integrate page routes correctly with sidebar

--- src/routes/PageRoutes/PageRoutes.jsx
+++ src/routes/PageRoutes/PageRoutes.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
 
 import SideBarMenu from "../../components/SideBarMenu";
 import TopBar from "../../components/TopBar";
 import Home from "./Home";
 
 import styles from "./styles.scss";
+import CourseList from "./CourseList";
 
-const PageRoutes = () => {
-  const menu = [{ name: "A" }, { name: "B" }];
+const PageRoutes = ({ match: { url } }) => {
+  const menu = [
+    { name: "Home", url: "/" },
+    { name: "Courses", url: "/courses" },
+  ];
 
   return (
     <>
       <TopBar />
       <div className={styles.container}>
-        <SideBarMenu menu={menu} />
+        <SideBarMenu menu={menu} currentUrl={url} />
         <div className={styles.content}>
-          <Router>
-            <Switch>
-              <Route exact path="/" component={Home} />
-            </Switch>
-          </Router>
+          <Switch>
+            <Route path={`${url}courses`} component={CourseList} />
+            <Route path={url} component={Home} />
+          </Switch>
         </div>
       </div>
     </>
