f6aa39124e483f8d1aff3300654f2aeab39307b9
Aldo Halim <aldohalim28@gmail.com>
2020-10-13 22:54:03 +0800

    [feature] login and registration page

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -2,11 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import PageRoutes from "./PageRoutes";
+import Login from "./Login";
+import Register from "./Register";
 
 const App = () => {
   return (
     <Router>
       <Switch>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
         <Route path="/" component={PageRoutes} />
       </Switch>
     </Router>
