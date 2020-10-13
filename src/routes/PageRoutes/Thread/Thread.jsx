d7f1b43889023ef2097936cefbe99ce974593e3b
Aldo Halim <aldohalim28@gmail.com>
2020-10-13 17:32:16 +0800

    [feature] thread page

---
+++ src/routes/PageRoutes/Thread/Thread.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import cx from "classnames";
+
+import QuestionBox from "../../../components/QuestionBox";
+import AnswerInput from "../../../components/AnswerInput";
+import AnswerBox from "../../../components/AnswerBox";
+
+import styles from "./styles.scss";
+import appStyles from "../../../stylesheets/app.scss";
+
+const Thread = () => {
+  const placeholderText =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed viverra lacus et varius tincidunt. Nam in aliquet felis, non consequat ex. Nunc tristique tincidunt consectetur. Aenean pulvinar condimentum dolor et vestibulum. Morbi rutrum purus commodo ligula accumsan, quis lacinia ligula auctor.";
+
+  const mockQuestion = {
+    id: 1,
+    name: "Name 0",
+    datePosted: "13/10 19:00",
+    title: "Question Title",
+    content: placeholderText,
+    courseCode: "CZ3002",
+    votes: 123,
+    userVote: 0,
+  };
+
+  const mockAnswers = [
+    {
+      id: 1,
+      name: "Name 1",
+      datePosted: "13/10 19:10",
+      content: placeholderText,
+      votes: 100,
+      verified: true,
+      userVote: 1,
+    },
+    {
+      id: 2,
+      name: "Name 2",
+      datePosted: "13/10 19:30",
+      content: placeholderText,
+      votes: 10,
+      verified: false,
+      userVote: -1,
+    },
+  ];
+
+  return (
+    <div
+      className={cx({
+        [appStyles.content_section]: true,
+        [styles.container]: true,
+      })}
+    >
+      <QuestionBox {...mockQuestion} />
+      <AnswerInput />
+      {mockAnswers.map((answer) => {
+        return <AnswerBox key={answer.id} {...answer} />;
+      })}
+    </div>
+  );
+};
+
+export default Thread;
