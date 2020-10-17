f0024def6d7b2e3f4ae727d2bc623e4c7f0894d5
Aldo Halim <aldohalim28@gmail.com>
2020-10-18 00:57:36 +0800

    [feature] integrate register with api, add logout feature

--- src/components/TopBar/TopBar.jsx
+++ src/components/TopBar/TopBar.jsx
@@ -1,18 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 import styles from "./styles.scss";
 import TextBox from "../TextBox";
+import * as action from "../../actions/user.action";
 
-const TopBar = () => {
+const TopBar = ({ logout }) => {
+  const history = useHistory();
+
+  const onLogoutClick = () => {
+    logout();
+    history.push("/login");
+  };
+
   return (
     <div className={styles.topbar}>
       <div className={styles.content}>
         <h2>NTUConnect</h2>
         <TextBox className={styles.search_bar} placeholder="Search" fullwidth />
         <p>Profile</p>
+        <p className={styles.logout} onClick={onLogoutClick}>
+          Logout
+        </p>
       </div>
     </div>
   );
 };
 
-export default TopBar;
+const mapDispatchToProps = {
+  logout: action.logout,
+};
+export default connect(null, mapDispatchToProps)(TopBar);
