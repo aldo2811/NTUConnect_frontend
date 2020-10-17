278d9c1defd71860fe1eff41354aca32b0cad485
Aldo Halim <aldohalim28@gmail.com>
2020-10-17 22:46:03 +0800

    [feature] add reducers for forums and threads, update user reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import user from "./user.reducer";
+import forums from "./forums.reducer";
+import threads from "./threads.reducer";
 
 export default combineReducers({
   user,
+  forums,
+  threads,
 });
