796df808177aafb4ecffd6fada53025fcd8e2bf5
Aldo Halim <aldohalim28@gmail.com>
2020-10-19 22:33:12 +0800

    [style] change date format on ui

--- src/components/UserBar/UserBar.jsx
+++ src/components/UserBar/UserBar.jsx
@@ -3,12 +3,41 @@ import React from "react";
 import styles from "./styles.scss";
 
 const UserBar = ({ username, datePosted }) => {
+  const monthMap = {
+    "01": "Jan",
+    "02": "Feb",
+    "03": "Mar",
+    "04": "Apr",
+    "05": "May",
+    "06": "Jun",
+    "07": "Jul",
+    "08": "Aug",
+    "09": "Sep",
+    10: "Oct",
+    11: "Nov",
+    12: "Dec",
+  };
+
+  const parseDate = (dateTime) => {
+    const datePattern = /\d{4}-\d{2}-\d{2}/;
+    const timePattern = /\d{2}:\d{2}:\d{2}/;
+    const date = dateTime.match(datePattern)[0];
+    const time = dateTime.match(timePattern)[0];
+
+    const [year, month, day] = date.split("-");
+    const [hour, minute] = time.split(":");
+
+    return `${day} ${monthMap[month]} ${year} • ${hour}:${minute}`;
+  };
+
   return (
     <div className={styles.user_bar}>
       <div>
         <b>{username}</b>
       </div>
-      <div className={styles.date_posted}>posted on {datePosted}</div>
+      <div className={styles.date_posted}>
+        posted on {parseDate(datePosted)}
+      </div>
     </div>
   );
 };
