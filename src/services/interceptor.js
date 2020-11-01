b973d3a9df11b333b9e734ec6d2c6d636e09fcf9
Aldo Halim <aldohalim28@gmail.com>
2020-11-02 02:24:05 +0800

    [refactor] remove logging for production

--- src/services/interceptor.js
+++ src/services/interceptor.js
@@ -7,13 +7,11 @@ import {
 axios.interceptors.response.use(
   (response) => {
     response.data = parseObjectToCamelCase(response.data);
-    console.log(response.data);
     return response;
   },
   (error) => {
     // eslint-disable-next-line no-param-reassign
     error.response = parseObjectToCamelCase(error.response);
-    console.log(error.response);
     return Promise.reject(error);
   }
 );
@@ -22,7 +20,6 @@ axios.interceptors.request.use(
   (config) => {
     // eslint-disable-next-line no-param-reassign
     config.data = parseObjectToSnakeCase(config.data);
-    console.log(config);
     return config;
   },
   (error) => {
--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,12 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import reduxThunk from "redux-thunk";
-import logger from "../middleware/logger";
 
 const store = (rootReducer) =>
-  createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(reduxThunk, logger))
-  );
+  createStore(rootReducer, composeWithDevTools(applyMiddleware(reduxThunk)));
 
 export default store;
